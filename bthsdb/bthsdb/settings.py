4625597f8ca4de636a63e2567380022d03554142
Goggleclub <Goggleclub@users.noreply.github.com>
2016-06-13 03:53:32 -0400

    changed DEBUG setting variable to False

--- bthsdb/bthsdb/settings.py
+++ bthsdb/bthsdb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g20#qinth9i=!#hfm($!m+^qm3lizmchzwu79q_1q^kqk1y1mw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
